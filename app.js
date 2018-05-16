bc0c55f87313302e278c2a8ebab541228b051529
Fernando Marinho <38432249+BlueSpeed7@users.noreply.github.com>
2018-05-16 17:56:39 -0300

    missing const json-server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const jsonServer = require('json-server');
 const router = jsonServer.router('https://my-json-server.typicode.com/BlueSpeed7/daycare-angular/db');
 const app = express();
 
