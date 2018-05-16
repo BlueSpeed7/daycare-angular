22f02f4a0a5ea2088913e68b0ff4a9b5ad7104e8
Fernando Marinho <38432249+BlueSpeed7@users.noreply.github.com>
2018-05-16 17:53:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-
+const router = jsonServer.router('https://my-json-server.typicode.com/BlueSpeed7/daycare-angular/db');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('https://my-json-server.typicode.com/BlueSpeed7/daycare-angular/db', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
+
+app.use(router);
 
 const port = process.env.PORT || '3000';
 app.set('port', port);
