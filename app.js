0fbe7410a03425122081c3c4b1641dc5d8aa6b54
Fernando <sunshinelopes@hotmail.com>
2018-05-16 20:24:45 -0300

    heroku ready

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const jsonServer = require('json-server');
-const router = jsonServer.router('https://my-json-server.typicode.com/BlueSpeed7/daycare-angular/db');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -10,8 +9,6 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
-
-app.use(router);
 
 const port = process.env.PORT || '3000';
 app.set('port', port);
