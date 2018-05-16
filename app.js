28dd80845fe48dc8b19c24b4f5db1bb9b7b75943
Fernando Marinho <38432249+BlueSpeed7@users.noreply.github.com>
2018-05-16 17:47:44 -0300

    fake server third commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('https://my-json-server.typicode.com/BlueSpeed7/daycare-angular/db', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
 
